dc3429f0eed5a03129a8308bbc88ab9946e991c9
irina <izhavoronkova@jinr.ru>
2026-04-28 15:12:33 +0300

    added new PID functions; tof planes and strips are readed properly from BmnTof400/700Hit; to exclude poorly calibrated tof strips, trBetaTof400/700_clean fields added, which are used to calculate trM2Tof400/700

--- convertBmn_run8.C
+++ convertBmn_run8.C
@@ -23,6 +23,10 @@ TF1 *f1_sigma_700_2212 = new TF1( "sigma_700_2212", "pol2", 0, 10 );
 TF1 *f1_m2_700_1000010020 = new TF1( "m2_700_1000010020", "pol1", 0, 10 );
 TF1 *f1_sigma_700_1000010020 = new TF1( "sigma_700_1000010020", "pol2", 0, 10 );
 
+double Poly3Poly3(double* xx, double* pp);
+double SkewGaus(double* xx, double* pp);
+double Poly3Gaus(double* xx, double* pp);
+
 std::vector<TGraph *> g1_a_lo_runid_vec;
 std::vector<TGraph *> g1_b_lo_runid_vec;
 std::vector<TGraph *> g1_a_up_runid_vec;
@@ -34,6 +38,9 @@ TGraph *g1_a_up_plane = nullptr;
 TGraph *g1_b_up_plane = nullptr;
 TGraph *g1_a_b_uTol = nullptr;
 Double_t a_dedx_calib, b_dedx_calib;
+
+std::map<int, std::vector<int>> tof400_strips_to_exclude_{ {0, {39, 40, 41, 42}}, {6, {19}}, {8, {29,30,31}}, {9, {15,16,26,27,28}},{12, {39, 40, 41}}, {16, {7, 8, 9, 10, 11, 12, 13, 14, 16, 19, 20, 21, 22, 23, 24, 25, 37}}, {18, {4, 13, 14, 15}}, {19, {35, 37}} };
+std::map<int, std::vector<int>> tof700_strips_to_exclude_{ {2, {25,26,}}, {3, {31}}, {5, {31}}, {6, {31}}, {8, {7,11,15,31}}, {9, {31}}, {10, {28,31}}, {12, {31}}, {13, {18,19,20,21,22,23,24,25,26,27,228,29,30,31}}, {14, {31}}, {16, {31}}, {17, {31}}, {18, {11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,31}}, {19, {31}}, {20,{31}}, {21, {13,14,15,23,24,25,27,30,31}}, {22, {31}}, {24, {31}}, {25, {31}}, {26, {6, 31}}, {27, {31}}, {28, {31}}, {29, {8,31}}, {30, {15,17,31}}, {31, {31}}, {32, {31}}, {33,{22,23,24,25,31}}, {34, {24,25,31}}, {35,{31}}, {36, {31}}, {37, {31}}, {38, {31}}, {39, {31}}, {40, {1,2,3,4,5,6,7,8,9,11,17,18,19,20,26,27,31}}, {45, {11,12,13,15}}, {49, {15}}, {52, {3}}, {55, {8,9}}, {57, {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}} };
 
 std::vector<int> physical_runs{6667, 6668, 6669, 6670, 6671, 6672, 6673, 6674, 6675, 6676, 6677, 6678, 6679, 6680, 6681, 6683, 6684, 6685, 666, 6687, 6689, 6690, 6691, 6692, 6694, 6695, 6696, 6698, 6699, 6732, 6733, 6734, 6737, 6738, 6739, 6740, 6745, 6752, 6753, 6760, 6761, 6765, 6766, 6767, 6768, 6769, 6771, 6772, 6773, 6774, 6779, 6780, 6782, 6783, 6785, 6786, 6788, 6794, 6795, 6797, 6799, 6800, 6803, 6815, 6816, 6817, 6818, 6819, 6820, 6821, 6822, 6879, 6882, 6883, 6884, 6886, 6887, 6889, 6891, 6900, 6901, 6902, 6903, 6904, 6905, 6906, 6907, 6908, 6909, 6910, 6911, 6915, 6916, 6918, 6919, 6920, 6921, 6923, 6924, 6926, 6927, 6928, 6929, 6930, 6931, 6932, 6933, 6934, 6935, 6936, 6937, 6939, 6940, 6968, 6970, 6972, 6973, 6975, 6976, 6977, 6978, 6979, 6980, 6981, 6982, 6983, 6984, 6990, 6991, 6992, 6993, 6994, 6995, 6997, 6998, 6999, 7000, 7002, 7003, 7004, 7005, 7006, 7008, 7009, 7010, 7011, 7012, 7030, 7031, 7032, 7033, 7034, 7035, 7037, 7038, 7040, 7041, 7042, 7043, 7044, 7046, 7047, 7048, 7049, 7050, 7051, 7052, 7053, 7054, 7055, 7056, 7075, 7076, 7077, 7078, 7081, 7082, 7083, 7084, 7086, 7087, 7091, 7092, 7093, 7094, 7096, 7097, 7098, 7100, 7101, 7102, 7103, 7104, 7125, 7126, 7127, 7128, 7129, 7130, 7131, 7132, 7133, 7135, 7136, 7137, 7138, 7146, 7149, 7150, 7151, 7154, 7155, 7156, 7157, 7159, 7160, 7161, 7162, 7163, 7164, 7165, 7166, 7167, 7168, 7173, 7174, 7175, 7176, 7177, 7178, 7179, 7180, 7181, 7182, 7184, 7186, 7187, 7188, 7191, 7192, 7193, 7194, 7195, 7200, 7202, 7203, 7205, 7206, 7207, 7208, 7209, 7211, 7212, 7213, 7214, 7215, 7216, 7217, 7218, 7219, 7220, 7223, 7225, 7255, 7258, 7261, 7263, 7265, 7267, 7268, 7269, 7271, 7272, 7274, 7276, 7278, 7279, 7281, 7284, 7286, 7288, 7290, 7291, 7312, 7313, 7320, 7321, 7322, 7323, 7325, 7326, 7327, 7328, 7337, 7342, 7343, 7344, 7345, 7346, 7348, 7349, 7351, 7352, 7353, 7354, 7355, 7356, 7357, 7358, 7359, 7361, 7363, 7364, 7365, 7367, 7369, 7374, 7376, 7377, 7378, 7379, 7380, 7381, 7382, 7386, 7387, 7388, 7389, 7390, 7391, 7392, 7393, 7395, 7396, 7397, 7398, 7399, 7400, 7401, 7402, 7403, 7405, 7406, 7408, 7409, 7410, 7411, 7412, 7413, 7414, 7415, 7417, 7418, 7419, 7421, 7422, 7423, 7425, 7427, 7428, 7429, 7431, 7432, 7433, 7434, 7435, 7437, 7439, 7440, 7441, 7442, 7444, 7445, 7446, 7447, 7449, 7451, 7452, 7453, 7454, 7455, 7456, 7457, 7458, 7460, 7461, 7469, 7471, 7472, 7473, 7474, 7477, 7478, 7480, 7481, 7482, 7483, 7484, 7487, 7488, 7489, 7490, 7491, 7492, 7493, 7495, 7497, 7498, 7500, 7501, 7502, 7513, 7514, 7515, 7517, 7519, 7520, 7521, 7528, 7529, 7530, 7531, 7532, 7533, 7534, 7537, 7538, 7539, 7542, 7543, 7545, 7546, 7547, 7549, 7550, 7551, 7552, 7553, 7554, 7564, 7565, 7566, 7567, 7569, 7570, 7572, 7573, 7574, 7575, 7577, 7579, 7581, 7584, 7585, 7586, 7587, 7590, 7591, 7592, 7596, 7597, 7599, 7600, 7604, 7605, 7606, 7607, 7608, 7609, 7611, 7612, 7613, 7622, 7623, 7625, 7626, 7627, 7628, 7630, 7631, 7633, 7634, 7635, 7636, 7638, 7639, 7640, 7641, 7643, 7644, 7645, 7646, 7647, 7649, 7655, 7656, 7657, 7659, 7660, 7662, 7663, 7664, 7665, 7666, 7668, 7669, 7670, 7671, 7673, 7674, 7675, 7676, 7677, 7678, 7679, 7681, 7682, 7684, 7685, 7687, 7688, 7689, 7690, 7692, 7693, 7694, 7696, 7698, 7700, 7701, 7702, 7703, 7704, 7705, 7710, 7712, 7713, 7714, 7715, 7716, 7717, 7718, 7721, 7723, 7724, 7725, 7726, 7727, 7728, 7729, 7730, 7732, 7733, 7734, 7735, 7736, 7737, 7751, 7752, 7753, 7755, 7756, 7761, 7762, 7763, 7764, 7766, 7767, 7768, 7769, 7771, 7772, 7775, 7776, 7778, 7779, 7780, 7781, 7783, 7784, 7785, 7786, 7788, 7789, 7790, 7791, 7794, 7795, 7796, 7797, 7798, 7801, 7802, 7803, 7814, 7816, 7819, 7821, 7824, 7825, 7828, 7829, 7830, 7831, 7832, 7834, 7835, 7836, 7842, 7843, 7845, 7846, 7847, 7848, 7850, 7851, 7852, 7853, 7855, 7856, 7857, 7858, 7859, 7865, 7868, 7869, 7870, 7871, 7873, 7874, 7876, 7877, 7878, 7880, 7882, 7883, 7884, 7885, 7886, 7887, 7890, 7891, 7892, 7893, 7894, 7896, 7897, 7898, 7899, 7900, 7901, 7903, 7904, 7905, 7906, 7907, 7908, 7910, 7911, 7912, 7913, 7914, 7931, 7932, 7933, 7935, 7937, 7938, 7939, 7941, 7942, 7944, 7948, 7949, 7950, 7952, 7954, 7955, 7957, 7958, 7960, 7961, 7962, 7963, 7965, 7966, 7967, 7975, 7977, 7978, 7979, 7981, 7982, 7986, 7988, 7989, 7990, 7991, 7992, 7995, 7996, 7997, 7998, 7999, 8000, 8001, 8002, 8004, 8005, 8006, 8007, 8008, 8009, 8013, 8014, 8015, 8016, 8018, 8020, 8021, 8022, 8023, 8026, 8027, 8028, 8029, 8030, 8031, 8032, 8033, 8038, 8039, 8040, 8041, 8042, 8044, 8045, 8046, 8047, 8048, 8050, 8051, 8052, 8053, 8055, 8056, 8057, 8058, 8059, 8061, 8063, 8064, 8065, 8066, 8068, 8069, 8070, 8071, 8072, 8074, 8075, 8076, 8077, 8079, 8080, 8081, 8082, 8084, 8086, 8087, 8088, 8089, 8090, 8097, 8100, 8101, 8102, 8104, 8106, 8108, 8109, 8110, 8111, 8112, 8113, 8115, 8116, 8117, 8118, 8119, 8121, 8122, 8123, 8124, 8129, 8130, 8131, 8133, 8137, 8138, 8139, 8140, 8141, 8142, 8144, 8156, 8157, 8158, 8159, 8160, 8161, 8162, 8165, 8166, 8167, 8168, 8169, 8170, 8173, 8174, 8175, 8176, 8177, 8180, 8183, 8184, 8186, 8188, 8190, 8191, 8192, 8193, 8195, 8196, 8198, 8199, 8201, 8202, 8203, 8204, 8205, 8206, 8207, 8208, 8209, 8210, 8211, 8212, 8213, 8215, 8217, 8219, 8220, 8221, 8228, 8229, 8230, 8231, 8235, 8236, 8238, 8239, 8240, 8242, 8244, 8245, 8246, 8247, 8248, 8250, 8251, 8253, 8254, 8255, 8256, 8257, 8258, 8265, 8266, 8267, 8268, 8270, 8271, 8273, 8274, 8275, 8276, 8277, 8278, 8279, 8281, 8284, 8286, 8287, 8288, 8289, 8290, 8292, 8293, 8294, 8295, 8297, 8298, 8299, 8300, 8305, 8306};
 std::vector<int> bad_runs{7313, 7415, 7417, 7435, 7469, 7517, 7519, 7520, 7537, 7575, 7604, 7630, 7657, 7659, 7679, 7681, 7705, 7735, 7843, 7847, 7848, 7850, 7851, 7852, 7853, 7855, 7856, 7857, 7858, 7859, 7865, 7868, 7907, 7931, 7932, 7933, 7935, 7937, 7938, 7939, 7954, 7955, 8031, 8032, 8033, 8115, 8121, 8167, 8201, 8204, 8205, 8208, 8209, 8210, 8211, 8212, 8213, 8215, 8247, 8265, 8266, 8267, 8281, 8289};
@@ -593,6 +600,43 @@ try {
   throw e;
 }
 
+auto TofHitPlane(const RVec<BmnTofHit>& tofHits)
+try
+{
+  vector<int> hit_mod_num;
+  hit_mod_num.reserve(tofHits.size());
+  for( const auto& hit : tofHits )
+  {
+    int mod = ((hit.GetDetectorID() & 0x0000FF00) >> 8) - 1;
+    hit_mod_num.push_back(mod);
+  }
+  return hit_mod_num;
+}
+catch(const std::exception &e)
+{
+    std::cout << __func__ << std::endl;
+    throw e;
+}
+
+auto TofHitStrip(const RVec<BmnTofHit>& tofHits)
+try
+{
+  vector<int> hit_strip_num;
+  hit_strip_num.reserve(tofHits.size());
+  for( const auto& hit : tofHits )
+  {
+    int strip = ((hit.GetDetectorID() & 0x000000FF)) - 1;
+    hit_strip_num.push_back(strip);
+  }
+  return hit_strip_num;
+}
+catch(const std::exception &e)
+{
+    std::cout << __func__ << std::endl;
+    throw e;
+}
+
+
 vector<XYZVector> recPos450(const RVec<BmnGlobalTrack> tracks)
 try {
   vector<XYZVector> pos;
@@ -623,7 +667,7 @@ try {
   auto testHit=(BmnTofHit*)hits.At(0);
   if (testHit)
   {
-    bool tof400 = testHit->GetZ()<600 ? true : false;
+    bool tof400 = testHit->GetZ()<550 ? true : false;
     for (auto track:tracks){
       int hitIndex = tof400 ? track.GetTof1HitIndex() : track.GetTof2HitIndex();
       if (hitIndex<0 || !hits.At(hitIndex)) 
@@ -639,7 +683,7 @@ try {
       TVector3 posAtHitZ(posAtHitZ_.x(), posAtHitZ_.y(), posAtHitZ_.z());
       TVector3 hitPos;
       hit->Position(hitPos);
-      auto posDiff = pos - posAtHitZ;
+      auto posDiff = hitPos - posAtHitZ; 
       res.push_back({posDiff.X(), posDiff.Y(), posDiff.Z()});
     }
   }
@@ -1177,7 +1221,10 @@ try {
   throw e;
 }
 
-void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", string inRunidDedxCalib = "run8_dedx_calibR_coeff.root", string inStsStationDedxCalib = "run8_dedx_calibS_coeff.root", std::string fileOut = "out.tree.root")
+void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", 
+                     string inRunidDedxCalib = "run8_dedx_calibR_coeff.root", string inStsStationDedxCalib = "run8_dedx_calibS_coeff.root", 
+                     string str_pid400_functions_file = "pars400_25.04.root", string str_pid700_functions_file = "pars700_25.04.root",
+                     std::string fileOut = "out.tree.root")
 {
   
   TChain *chainRec=makeChain(inReco, "bmndata");
@@ -1297,8 +1344,6 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
   b_dedx_calib = g1_a_b_uTol->GetPointY(1);
   //dEdx calib
 
-
-
   //magField = new BmnNewFieldMap("field_sp41v5_ascii_Extrap.root");
   //FieldMap_1900_extrap_noPed
   magField = new BmnNewFieldMap("FieldMap_1900_extrap_noPed.root");
@@ -1309,6 +1354,70 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
   auto hodoModPos=modulePos(geoFileName.Data(),"hodo");
   auto fhcalModPos=modulePos(geoFileName.Data(),"fhcal");
 
+
+	TF1* f1_211_m_400 = nullptr;
+  TF1* f1_211_s_400 = nullptr;
+  TF1* f1_211_m_700_proto = nullptr;
+  TF1* f1_211_s_700_proto = nullptr;
+
+  TF1* f1_2212_m_400 = nullptr;
+  TF1* f1_2212_s_400 = nullptr;
+  TF1* f1_2212_m_700 = nullptr;
+  TF1* f1_2212_s_700 = nullptr;
+  	
+	TF1* f1_1000010020_m_400_proto = nullptr;
+  TF1* f1_1000010020_s_400 = nullptr;
+  TF1* f1_1000010020_m_700_proto = nullptr;
+  TF1* f1_1000010020_s_700 = nullptr;
+
+  TF1* f1_1000010020_m_400 = new TF1("f1_1000010020_m_400", Poly3Poly3, 0., 10., 9);
+	TF1* f1_211_m_700 = new TF1("f1_211_m_700", Poly3Gaus, 0., 10., 9);
+  TF1* f1_211_s_700 = new TF1("f1_211_m_700", SkewGaus, 0., 10., 4);
+  TF1* f1_1000010020_m_700 = new TF1("f1_1000010020_m_700", Poly3Poly3, 0., 10., 9);
+
+  auto file_pid400 = TFile::Open( str_pid400_functions_file.c_str(), "READ" );
+	file_pid400->GetObject("fit_211_x0", f1_211_m_400);
+	file_pid400->GetObject("fit_211_sigma", f1_211_s_400);
+	file_pid400->GetObject("fit_2212_x0", f1_2212_m_400);
+	file_pid400->GetObject("fit_2212_sigma", f1_2212_s_400);
+  file_pid400->GetObject("fit_1000010020_sigma", f1_1000010020_s_400);
+  file_pid400->GetObject("fit_1000010020_x0", f1_1000010020_m_400_proto);
+  double* pars400_de_x0 = new double[f1_1000010020_m_400_proto->GetNpar()];
+  f1_1000010020_m_400_proto->GetParameters(pars400_de_x0);
+  f1_1000010020_m_400->SetParameters(pars400_de_x0);
+
+ 	auto file_pid700 = TFile::Open( str_pid700_functions_file.c_str(), "READ" );
+
+  file_pid700->GetObject("fit_211_x0", f1_211_m_700_proto);
+ 	double* pars700_211_x0 = new double[f1_211_m_700_proto->GetNpar()];
+ 	f1_211_m_700_proto->GetParameters(pars700_211_x0);
+ 	f1_211_m_700->SetParameters(pars700_211_x0);
+	
+	file_pid700->GetObject("fit_211_sigma", f1_211_s_700_proto);
+	double* pars700_211_s = new double[f1_211_s_700_proto->GetNpar()];
+	f1_211_s_700_proto->GetParameters(pars700_211_s);
+	f1_211_s_700->SetParameters(pars700_211_s);
+
+	file_pid700->GetObject("fit_2212_x0", f1_2212_m_700);
+	file_pid700->GetObject("fit_2212_sigma", f1_2212_s_700);
+  file_pid700->GetObject("fit_1000010020_sigma", f1_1000010020_s_700);
+
+  file_pid700->GetObject("fit_1000010020_x0", f1_1000010020_m_700_proto);
+  double* pars700_de_x0 = new double[f1_1000010020_m_700_proto->GetNpar()];
+  f1_1000010020_m_700_proto->GetParameters(pars700_de_x0);
+  f1_1000010020_m_700->SetParameters(pars700_de_x0);
+
+  delete [] pars400_de_x0;
+  delete [] pars700_211_x0;
+  delete [] pars700_211_s;
+  delete [] pars700_de_x0;
+  delete f1_1000010020_m_400_proto;
+  delete f1_1000010020_m_700_proto;
+	
+  file_pid400->Close();
+  file_pid700->Close();
+
+/*
   //pion TOF-400
   //TF1 *f1_m2_400_211 = new TF1( "m2_400_211", "pol1", 0, 10 );
   f1_m2_400_211->SetParameter( 0, 0.03911 );
@@ -1366,6 +1475,7 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
   f1_sigma_700_1000010020->SetParameter( 0, 0.2818 );
   f1_sigma_700_1000010020->SetParameter( 1, -0.111 );
   f1_sigma_700_1000010020->SetParameter( 2, 0.03891 );
+*/
 
   auto dd=d
     // pileup
@@ -1491,8 +1601,8 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
     .Define("stsTrackNdf", stsTrackNdf, { "BmnGlobalTrack", "StsVector" })
     .Define("stsTrackNhits", stsTrackNhits, { "BmnGlobalTrack", "StsVector" })
     //nSigma from TOF-400 and TOF-700
-    .Define("trM2Tof400",trM2,{"trMom","trBetaTof400"})
-    .Define("trM2Tof700",trM2,{"trMom","trBetaTof700"})
+    // .Define("trM2Tof400",trM2,{"trMom","trBetaTof400"})
+    // .Define("trM2Tof700",trM2,{"trMom","trBetaTof700"})
     //dEdx
     .Define("trEnergyLoss", trEnergyLoss(run_id, is_physical_run), {"BmnGlobalTrack", "StsVector", "StsHit", "BmnGemLowerCluster", "BmnGemUpperCluster", "BmnSiliconLowerCluster", "BmnSiliconUpperCluster"})
     //beam track
@@ -1505,8 +1615,8 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
     .Define("beamTrackParameters", BeamTrackParameters, { "BmnBeamTrack" })
 
     .Define("tof400Digits","TOF400.fUniqueID")
-    .Define("tof400Plane","TOF400.fPlane")
-    .Define("tof400Strip","TOF400.fStrip")
+    .Define("tof400Plane", TofHitPlane, {"BmnTof400Hit"})
+    .Define("tof400Strip", TofHitStrip, {"BmnTof400Hit"})
     .Define("tof400hitPos",tofHitPosition,{"BmnTof400Hit"})
     .Define("tof400hitT","BmnTof400Hit.fTimeStamp")
     .Define("tof400hitL","BmnTof400Hit.fLength")
@@ -1516,8 +1626,8 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
     .Define("tof400hitResCalc",tofRes,{"BmnGlobalTrack","BmnTof400Hit"})
 
     .Define("tof700Digits","TOF700.fUniqueID")
-    .Define("tof700Plane","TOF700.fPlane")
-    .Define("tof700Strip","TOF700.fStrip")
+    .Define("tof700Plane", TofHitPlane, {"BmnTof700Hit"})
+    .Define("tof700Strip", TofHitStrip, {"BmnTof700Hit"})
     .Define("tof700hitPos",tofHitPosition,{"BmnTof700Hit"})
     .Define("tof700hitT","BmnTof700Hit.fTimeStamp")
     .Define("tof700hitL","BmnTof700Hit.fLength")
@@ -1525,6 +1635,11 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
     .Define("tof700hitResY","BmnTof700Hit.fResY")
     .Define("tof700hitRefIndex","BmnTof700Hit.fRefIndex")
     .Define("tof700hitResCalc",tofRes,{"BmnGlobalTrack","BmnTof700Hit"})
+
+    .Define("trBetaTof400_clean", remove_beta400_from_bad_strips, {"trBetaTof400", "trTof400Hit", "tof400Plane", "tof400Strip"})
+    .Define("trBetaTof700_clean", remove_beta700_from_bad_strips, {"trBetaTof700", "trTof400Hit", "tof700Plane", "tof700Strip"})
+    .Define("trM2Tof400", trM2, {"trMom", "trBetaTof400_clean"})
+    .Define("trM2Tof700", trM2, {"trMom", "trBetaTof700_clean"})
 
     .Define("scwallModPos",[scwallModPos](){return scwallModPos;})
     .Define("scwallModId",moduleId, {"scwallModPos"})
@@ -1564,4 +1679,52 @@ void convertBmn_run8(string inReco="reco.root", string inDigi="digi.root", strin
   dd.Snapshot("t", fileOut, definedNames);
 
   std::cout<<"Convert_done"<<std::endl;
+}
+
+double Poly3Poly3(double* xx, double* pp)
+{
+
+      double threshold = pp[0];
+      double a_pol3 = pp[1];
+      double b_pol3 = pp[2];
+      double c_pol3 = pp[3];
+      double d_pol3 = pp[4];
+      double a_pol3_2 = pp[5];
+      double b_pol3_2 = pp[6];
+      double c_pol3_2 = pp[7];
+      double d_pol3_2 = pp[8];
+
+      return xx[0] < threshold ? a_pol3*pow(xx[0],3) + b_pol3*pow(xx[0],2) + c_pol3*pow(xx[0],1) + d_pol3 : a_pol3_2*pow(xx[0],3) + b_pol3_2*pow(xx[0],2) + c_pol3_2*pow(xx[0],1) + d_pol3_2;
+}
+
+double SkewGaus(double* xx, double* pp)
+{
+    auto A = pp[0];
+    auto mean = pp[1];
+    auto sigma = pp[2];
+    auto alpha = pp[3];
+    auto norm = A / (sqrt(2. * TMath::Pi()) * sigma);
+    auto arg = (xx[0] - mean) / sigma;
+    auto phi = TMath::Gaus(arg, 0.0, 1.0, true);
+    auto Phi = 0.5 * (1 + std::erf(alpha * arg / std::sqrt(2)));
+    return norm * phi * Phi;
+}
+
+double Poly3Gaus(double* xx, double* pp)
+{
+    double threshold = pp[0];
+    double amplitude = pp[1];
+    double mean = pp[2];
+    double sigma = pp[3];
+    double a_pol3 = pp[4];
+    double b_pol3 = pp[5];
+    double c_pol3 = pp[6];
+    double d_pol3 = pp[7];
+    double l = pp[8];
+    double norm = amplitude / (sqrt(2. * TMath::Pi()) * sigma);
+    double arg = (xx[0] - mean) / sigma;
+    double smallphi = TMath::Gaus(arg, 0.0, 1.0, true);
+
+    return xx[0] < threshold ? a_pol3*pow(xx[0],3) + b_pol3*pow(xx[0],2) + c_pol3*pow(xx[0],1) + d_pol3 : norm * smallphi + l;
+      
 }
